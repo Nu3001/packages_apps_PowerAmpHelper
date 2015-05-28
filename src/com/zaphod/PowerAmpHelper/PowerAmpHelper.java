7970fc8d668b2f4e0468fa7f3183097326d90a19
Mike Wilson <mwilson@acesappraisal.com>
2015-05-28 07:58:13 -0400

    first commit

---
+++ src/com/zaphod/PowerAmpHelper/PowerAmpHelper.java
@@ -0,0 +1,40 @@
+package com.zaphod.PowerAmpHelper;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.lang.Override;
+
+public class PowerAmpHelper extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        PackageManager pm = getPackageManager();
+        Intent launch = pm.getLaunchIntentForPackage("com.maxmpz.audioplayer");
+        try {
+            startActivity (launch);
+        } catch(ActivityNotFoundException anf) {
+            finish();
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    
+}
